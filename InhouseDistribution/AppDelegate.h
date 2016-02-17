5cfc18ea599ef1991bb8b0192c3452866e6cdba2
huliyou <liyoupie@gmail.com>
2016-02-17 22:25:53 +0800

    Initial Commit

---
+++ InhouseDistribution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InhouseDistribution
+//
+//  Created by Leiyou Who on 2/17/16.
+//  Copyright © 2016 Leiyou Who. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InhouseDistribution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InhouseDistribution
+//
+//  Created by Leiyou Who on 2/17/16.
+//  Copyright © 2016 Leiyou Who. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
